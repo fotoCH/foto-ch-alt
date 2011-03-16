ec63a1077ec566de7907c42455aa50f4d0ee7055
chrigu <chrigu@foto-ch.ch>
2011-03-16 15:16:44 +0000

    ref #29 session log logs useragent firstpage

--- fotoch/log.inc.php
+++ fotoch/log.inc.php
@@ -2,7 +2,9 @@
 
 function log_session(){
 	$sid = session_id();
-	$sql="INSERT INTO log_sessions (`session_id`, `start`, `last`) VALUES ('".$sid."',NOW(),NOW())";
+	$ua=mysql_real_escape_string($_SERVER['HTTP_USER_AGENT']);
+	$req=mysql_real_escape_string($_SERVER['REQUEST_URI']);
+	$sql="INSERT INTO log_sessions (`session_id`, `start`, `last`,`useragent`,`firstpage`) VALUES ('".$sid."',NOW(),NOW(),'".$ua."','".$req."')";
 	mysql_query($sql);
 	$sql="UPDATE log_sessions SET `last`=NOW(), `count`=`count`+1, `seconds`=TIMESTAMPDIFF(SECOND,`start`,`last`)  WHERE `session_id`='".$sid."'";
 	//echo $sql;	
