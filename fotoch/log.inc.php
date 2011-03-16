e66b8e7394d3e57dd8e7a2091273a6d13a46ac1c
chrigu <chrigu@foto-ch.ch>
2011-03-16 16:18:24 +0000

    ref #29 log botdetection

--- fotoch/log.inc.php
+++ fotoch/log.inc.php
@@ -1,28 +1,55 @@
 <?php
+function isbot() {
+	// Get current User-Agent
+	$current = strtolower( $_SERVER['HTTP_USER_AGENT'] );
+
+	// Array of known bot lowercase strings
+	// Example: 'googlebot' will match 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)'
+	$bots = array(
+	// general web bots
+                'googlebot', 'yahoo! slurp',
+                'dotbot', 'yeti', 'http://help.naver.com/robots/', 'scoutjet', 
+                'http://yandex.com/bots', 'linkedinbot', 'mj12bot', 'http://www.80legs.com/spider.html',
+                'exabot', 'msnbot', 'yacybot', 'www.oneriot.com', 'http://flipboard.com/', 
+                'baiduspider', 'mxbot', 'bingbot',
+
+	// twitter specific bots
+                'http://thinglabs.com', 'js-kit url resolver', 'twitterbot', 'njuicebot', 'postrank.com',
+                'tweetmemebot', 'longurl api', 'paperlibot', 'http://postpo.st/crawlers',
+	);
+
+	// Check if the current UA string contains a know bot string
+	$is_bot = ( str_replace( $bots, '', $current ) != $current );
+
+	return $is_bot;
+}
 
 function log_session(){
 	$sid = session_id();
 	$ua=mysql_real_escape_string($_SERVER['HTTP_USER_AGENT']);
 	$req=mysql_real_escape_string($_SERVER['REQUEST_URI']);
-	$sql="INSERT INTO log_sessions (`session_id`, `start`, `last`,`useragent`,`firstpage`) VALUES ('".$sid."',NOW(),NOW(),'".$ua."','".$req."')";
+	$isbot=isbot()?1:2;
+	$sql="INSERT INTO log_sessions (`session_id`, `start`, `last`,`useragent`,`firstpage`,`isbot`) VALUES ('".$sid."',NOW(),NOW(),'".$ua."','".$req."',$isbot)";
 	mysql_query($sql);
 	$sql="UPDATE log_sessions SET `last`=NOW(), `count`=`count`+1, `seconds`=TIMESTAMPDIFF(SECOND,`start`,`last`)  WHERE `session_id`='".$sid."'";
-	//echo $sql;	
+	//echo $sql;
 	mysql_query($sql);
 }
 
 function log_setLevel(){
 	$sid = session_id();
 	$sql="UPDATE log_sessions SET `level`=".$_SESSION['usr_level']."  WHERE `session_id`='".$sid."'";
-	//echo $sql;	
+	//echo $sql;
 	mysql_query($sql);
 }
 
 function log_page($kategorie,$search,$action,$lang,$level,$url){
 	$action=mysql_real_escape_string($action);
 	$url=mysql_real_escape_string($url);
-	$sql="INSERT INTO log_pages (`kategorie`,`search`,`action`,`lang`,`level`,`url`) VALUES ('$kategorie',$search,'$action','$lang','$level','$url')";
+	$isbot=isbot()?1:3;
+	$sql="INSERT INTO log_pages (`kategorie`,`search`,`action`,`lang`,`level`,`url`,`isbot`) VALUES ('$kategorie',$search,'$action','$lang','$level','$url',$isbot)";
 	mysql_query($sql);
 }
+//$sql = "SELECT COUNT(id),useragent,level FROM `log_sessions` WHERE 1 GROUP BY useragent,level ORDER BY level,useragent LIMIT 0, 30 ";
 
 ?>
