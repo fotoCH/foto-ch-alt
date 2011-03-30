44e6822f4d6ba15362b63e18a595e74043784337
chrigu <chrigu@foto-ch.ch>
2011-03-30 22:04:59 +0000

    fix alpha suche

--- fotoch/sites/ausstellung/ausstellungresults.php
+++ fotoch/sites/ausstellung/ausstellungresults.php
@@ -31,41 +31,47 @@ else {
 		$result=mysql_query("SELECT * FROM ausstellung WHERE titel LIKE '$anf%' ORDER BY  titel Asc");
 		$issearch=2;
 	}
-	elseif($volltext!='') {
-		if (auth_level(USER_GUEST_READER)){
-			$w1="(titel LIKE '%$volltext%' OR institution LIKE '%$volltext%' OR ort LIKE '%$volltext%' OR `jahr`  LIKE '%$volltext%' OR `institution`  LIKE '%$volltext%' OR `text_alt` LIKE '%$volltext%' OR `notiz` LIKE '%$volltext%')";
-			$sql = "SELECT * FROM ausstellung WHERE  ORDER BY jahr DESC";
+	else {
+		if($volltext!='') {
+			if (auth_level(USER_GUEST_READER)){
+				$w1="(titel LIKE '%$volltext%' OR institution LIKE '%$volltext%' OR ort LIKE '%$volltext%' OR `jahr`  LIKE '%$volltext%' OR `institution`  LIKE '%$volltext%' OR `text_alt` LIKE '%$volltext%' OR `notiz` LIKE '%$volltext%')";
+				$sql = "SELECT * FROM ausstellung WHERE  ORDER BY jahr DESC";
+			}
+			else {
+				$w1="titel LIKE '%$volltext%' OR institution LIKE '%$volltext%' OR ort LIKE '%$volltext%' OR `jahr`  LIKE '%$volltext%' OR `institution`  LIKE '%$volltext%'";
+				
+			}
 		}
 		else {
-			$w1="titel LIKE '%$volltext%' OR institution LIKE '%$volltext%' OR ort LIKE '%$volltext%' OR `jahr`  LIKE '%$volltext%' OR `institution`  LIKE '%$volltext%'";
-			
+			//echo "fehler";
 		}
-	}
-	else {
-		//echo "fehler";
-	}
-	$where='';
-	foreach (array('institution','ort','jahr') as $key){
-		if (!empty($_GET[$key])){
+		$where='';
+		foreach (array('institution','ort','jahr') as $key){
+			if (!empty($_GET[$key])){
+				if (!empty($where)){
+					$where.=" AND ";
+				}
+				$where.="$key LIKE '%".$_GET[$key]."%' ";
+			}
+		}
+		
+		if ($w1){
 			if (!empty($where)){
 				$where.=" AND ";
 			}
-			$where.="$key LIKE '%".$_GET[$key]."%' ";
+			$where.=$w1;
 		}
+		$sql = "SELECT * FROM ausstellung WHERE $where ORDER BY jahr DESC";
+		$issearch=3;
+		$result=mysql_query($sql);
 	}
-	
-	if ($w1){
-		if (!empty($where)){
-			$where.=" AND ";
-		}
-		$where.=$w1;
-	}
-	$sql = "SELECT * FROM ausstellung WHERE $where ORDER BY jahr DESC";
-	$issearch=3;
-	$result=mysql_query($sql);
-	
 }
 while($fetch=mysql_fetch_array($result)){
+
+	
+	if (trim($fetch['titel'])==''){
+		$fetch['titel']='&ndash;';  // platzhalter für link
+	}
 	$def->assign("FETCH",$fetch);
 	$def->parse("list.row_ausstellung".$admin);
 }
