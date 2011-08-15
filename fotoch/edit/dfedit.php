c79b9c8b9bfac607530cfe4d5980f99d292f529b
oli <oli@foto-ch.ch>
2011-08-15 13:25:27 +0000

    add df save for institutionen

--- fotoch/edit/dfedit.php
+++ fotoch/edit/dfedit.php
@@ -42,75 +42,70 @@ if ($fertig==1){
 		$result = mysql_query($sql);
 		$array_eintrag = mysql_fetch_array($result);
 		//print_r($_POST);
-		if ($type=='fotografen'){
-			foreach ($_REQUEST['dokumentation'] as $t){
-				$dokumentation .=$t;
-				$dokumentation .=",";
-			}
-			$dokumentation = substr($dokumentation, 0, strlen($dokumentation)-1);
-			$_POST['dokumentation']=$dokumentation;
+		foreach ($_REQUEST['dokumentation'] as $t){
+			$dokumentation .=$t;
+			$dokumentation .=",";
+		}
+		$dokumentation = substr($dokumentation, 0, strlen($dokumentation)-1);
+		$_POST['dokumentation']=$dokumentation;
 
-			$fs=array("projektname", "territoriumszugegoerigkeit", "bearbeitungstiefe","dokumentation","dokumentation_text","notiz_fiche");
-			$refs=array("biografie", "ausstellungen", "auszeichnungen_stipendien", "bestaende", "interview_vorgesehen", "interview_fertiggestellt","dokumentation");
-			$wrefs=array("werdegang", "schaffensbeschrieb", "uebersetzung_de", "uebersetzung_fr", "uebersetzung_it", "uebersetzung_rm", "uebersetzung_en");
-			$sql="UPDATE  $type SET ";
-			$s='';
-			$s2=''; // für history
-			foreach ($fs as $t){
-				$u=($_POST[$t]==$array_eintrag[$t]?'':'`'.$t.'`=\''.mysql_real_escape_string($_POST[$t])."'");
+		$fs=array("projektname", "territoriumszugegoerigkeit", "bearbeitungstiefe","dokumentation","dokumentation_text","notiz_fiche");
+		$refs=array("biografie", "ausstellungen", "auszeichnungen_stipendien", "bestaende", "interview_vorgesehen", "interview_fertiggestellt","dokumentation");
+		$wrefs=array("werdegang", "schaffensbeschrieb", "uebersetzung_de", "uebersetzung_fr", "uebersetzung_it", "uebersetzung_rm", "uebersetzung_en");
+		$sql="UPDATE  $type SET ";
+		$s='';
+		$s2=''; // für history
+		foreach ($fs as $t){
+			$u=($_POST[$t]==$array_eintrag[$t]?'':'`'.$t.'`=\''.mysql_real_escape_string($_POST[$t])."'");
+			if ($u){
+				$s.=($s?', ':'').$u;
+				$s2.=($s2?', ':'').getHChanged($t,$_POST[$t],$array_eintrag[$t]);
+			}
+		}
+		foreach ($refs as $t){
+			$pdate=rformdate($_POST[$t.'_date']);
+			if ($_POST[$t.'_date']){
+				$u=($pdate==$array_eintrag[$t.'_date']?'':'`'.$t.'_date'.'`=\''.mysql_real_escape_string($pdate)."'");
 				if ($u){
 					$s.=($s?', ':'').$u;
-					$s2.=($s2?', ':'').getHChanged($t,$_POST[$t],$array_eintrag[$t]);
+					$s2.=($s2?', ':'').getHChanged($t.'_date',$pdate,$array_eintrag[$t.'_date']);
 				}
-			}
-			foreach ($refs as $t){
-				$pdate=rformdate($_POST[$t.'_date']);
-				if ($_POST[$t.'_date']){
-					$u=($pdate==$array_eintrag[$t.'_date']?'':'`'.$t.'_date'.'`=\''.mysql_real_escape_string($pdate)."'");
-					if ($u){
-						$s.=($s?', ':'').$u;
-						$s2.=($s2?', ':'').getHChanged($t.'_date',$pdate,$array_eintrag[$t.'_date']);
-					}
 
-					$u=($_POST[$t.'_user']==$array_eintrag[$t.'_user']?'':'`'.$t.'_user'.'`=\''.mysql_real_escape_string($_POST[$t.'_user'])."'");
+				$u=($_POST[$t.'_user']==$array_eintrag[$t.'_user']?'':'`'.$t.'_user'.'`=\''.mysql_real_escape_string($_POST[$t.'_user'])."'");
+				if ($u){
+					$s.=($s?', ':'').$u;
+					$s2.=($s2?', ':'').getHChanged($t.'_user',getusername($_POST[$t.'_user']),getusername($array_eintrag[$t.'_user']));
+				}
+			} else {
+				if ($array_eintrag[$t.'_date']!='' && $array_eintrag[$t.'_date']!='0000-00-00'){ //Eintrag gelöscht
+					//echo "old: ".$array_eintrag[$t.'_date']."<br />";
+					$u='`'.$t.'_date'.'`=\''."'";
 					if ($u){
 						$s.=($s?', ':'').$u;
-						$s2.=($s2?', ':'').getHChanged($t.'_user',getusername($_POST[$t.'_user']),getusername($array_eintrag[$t.'_user']));
-					}
-				} else {
-					if ($array_eintrag[$t.'_date']!='' && $array_eintrag[$t.'_date']!='0000-00-00'){ //Eintrag gelöscht
-						//echo "old: ".$array_eintrag[$t.'_date']."<br />";
-						$u='`'.$t.'_date'.'`=\''."'";
-						if ($u){
-							$s.=($s?', ':'').$u;
-							$s2.=($s2?', ':'').$t.' deleted';
-						}
+						$s2.=($s2?', ':'').$t.' deleted';
 					}
 				}
 			}
-			foreach ($wrefs as $t){
-				for ($l=0;$l<=6;$l++){
-					$pdate=rformdate($_POST[$t.'_'.$l.'_date']);
-					if ($_POST[$t.'_'.$l.'_date']){
-						$u=($pdate==$array_eintrag[$t.'_l'.$l.'_date']?'':'`'.$t.'_l'.$l.'_date'.'`=\''.mysql_real_escape_string($pdate)."'");
-						if ($u){
-							$s.=($s?', ':'').$u;
-							$s2.=($s2?', ':'').getHChanged($t.'_l'.$l.'_date',$pdate,$array_eintrag[$t.'_l'.$l.'_date']);
-						}
-						if ($_POST[$t.'_'.$l.'_userbox']!=0){
-							$u=($_POST[$t.'_'.$l.'_userbox']==$array_eintrag[$t.'_l'.$l.'_user']?'':'`'.$t.'_l'.$l.'_user'.'`=\''.mysql_real_escape_string($_POST[$t.'_'.$l.'_userbox'])."'");
-							if ($u){
-								$s.=($s?', ':'').$u;
-								$s2.=($s2?', ':'').getHChanged($t.'_l'.$l.'_user',getusername($_POST[$t.'_'.$l.'_userbox']),getusername($array_eintrag[$t.'_l'.$l.'_user']));
-							}
-						}
-					}
-				}
-			}
-		} else {  // type== institution
-				
 		}
-
+		foreach ($wrefs as $t){
+			for ($l=0;$l<=6;$l++){
+				$pdate=rformdate($_POST[$t.'_'.$l.'_date']);
+				if ($_POST[$t.'_'.$l.'_date']){
+					$u=($pdate==$array_eintrag[$t.'_l'.$l.'_date']?'':'`'.$t.'_l'.$l.'_date'.'`=\''.mysql_real_escape_string($pdate)."'");
+					if ($u){
+						$s.=($s?', ':'').$u;
+						$s2.=($s2?', ':'').getHChanged($t.'_l'.$l.'_date',$pdate,$array_eintrag[$t.'_l'.$l.'_date']);
+					}
+					if ($_POST[$t.'_'.$l.'_userbox']!=0){
+						$u=($_POST[$t.'_'.$l.'_userbox']==$array_eintrag[$t.'_l'.$l.'_user']?'':'`'.$t.'_l'.$l.'_user'.'`=\''.mysql_real_escape_string($_POST[$t.'_'.$l.'_userbox'])."'");
+						if ($u){
+							$s.=($s?', ':'').$u;
+							$s2.=($s2?', ':'').getHChanged($t.'_l'.$l.'_user',getusername($_POST[$t.'_'.$l.'_userbox']),getusername($array_eintrag[$t.'_l'.$l.'_user']));
+						}
+					}
+				}
+			}
+		}
 
 		$sql.=$s." WHERE id ='$id'";
 
@@ -150,6 +145,11 @@ if ($fertig==1){
 		 `kanton` = '$kanton',
 		 `bildgattungen_set` = '$bildgattungen_set' WHERE `id` ='$_POST[hidden_id]' LIMIT 1"; */
 		$result = mysql_query($sql);
+		if( !$result ) {
+			echo "error updating dokufiche:<br>";
+			echo "$sql<br>";
+			echo mysql_error();
+		}
 	}
 
 	//////////////Formdaten aus Tabelle 'fotografen'  holen////////////////////////////
