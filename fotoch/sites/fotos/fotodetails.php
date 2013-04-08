944bbe1ea0c67ee78d3d7f1222ce08ffe949857b
chrigu <chrigu@foto-ch.ch>
2013-04-08 13:47:04 +0000

    Institution und Bestand vor Fotodetail

--- fotoch/sites/fotos/fotodetails.php
+++ fotoch/sites/fotos/fotodetails.php
@@ -8,7 +8,7 @@ $xtpl_fotodetails->assign("LANG", $_GET['lang']);
 $xtpl_fotodetails->assign("TITLE", $spr['photos']);
 $xtpl_fotodetails->assign("SPR",$spr);
 
-$select = 'i.id AS institution_id, b.id AS stock_id, n.fotografen_id AS photograph_id, f.dc_title AS titel, f.dc_description AS description, CONCAT(n.vorname, " ", n.nachname) AS fotograph, f.dc_created AS zeitraum, f.dc_coverage AS coverage, b.name AS bestand, i.name AS institution, f.dc_right AS copy, f.dcterms_medium AS medium, f.dc_identifier AS img_url, f.image_path, f.dcterms_spatial AS keywords, f.dcterms_subject AS subject';
+$select = 'i.id AS institution_id, b.id AS stock_id, n.fotografen_id AS photograph_id, f.dc_title AS titel, f.dc_description AS description, CONCAT(n.vorname, " ", n.nachname) AS fotograph, f.dc_created AS zeitraum, f.dc_coverage AS coverage, i.name AS institution, b.name AS bestand, f.dc_right AS copy, f.dcterms_medium AS medium, f.dc_identifier AS img_url, f.image_path, f.dcterms_spatial AS keywords, f.dcterms_subject AS subject';
 
 $join .= "LEFT JOIN namen AS n ON f.dc_creator=n.fotografen_id ";
 $join .= "LEFT JOIN institution AS i ON f.edm_dataprovider=i.id ";
@@ -52,12 +52,14 @@ while($arrResult=mysql_fetch_assoc($objResult)){
                     break;
                 }
                 $value = '<a href="?a=institution&id='.$institutionID.'&lang='.($lang? $lang : 'de').'">'.$value.'</a>';
+                $instbest=$value.': ';
                 break;
             case 'bestand':
                 if ($value == $spr['not_available']){
                     break;
                 }
                 $value = '<a href="?a=bestand&id='.$stockID.'&lang='.($lang? $lang : 'de').'">'.$value.'</a>';
+                $instbest.=$value;
                 break;
             case 'img_url':
                 $value = '<a href="'.$value.'" target="_blank">'.$value.'</a>';
@@ -85,6 +87,7 @@ while($arrResult=mysql_fetch_assoc($objResult)){
 }
 
 // render the photo
+$xtpl_fotodetails->assign("InstBest", $instbest);
 $xtpl_fotodetails->assign("photo_src", $photoPath);
 $xtpl_fotodetails->assign("photo_alt", $img_alt);
 $xtpl_fotodetails->parse('autodetail.photo');
