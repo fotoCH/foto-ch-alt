4a7c6c2d258231d7ad137487ecc3056e9d96fe8c
beni <beni@foto-ch.ch>
2013-11-28 13:14:47 +0000

    don't show maps over search results

--- fotoch/sites/newsearch/newsearch.php
+++ fotoch/sites/newsearch/newsearch.php
@@ -25,7 +25,8 @@ else {
 	$issearch=1;
 	include("searchresults.php");
 	$bestand->assign("LIST", $results);
-	$bestand->parse("contents.search.map");
+	if(!isset($_GET[seg]) && !isset($_GET['volltext']))
+		$bestand->parse("contents.search.map");
 	$bestand->parse("contents.search");
 
 	$out.=$bestand->text("contents.search");
