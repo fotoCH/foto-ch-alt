303e19715010fa0c3b68a3a2fdb5cde60e1649a7
chrigu <chrigu@foto-ch.ch>
2015-03-04 14:16:21 +0000

    ajax for typeahead

---
+++ fotoch/api/ftas.php
@@ -0,0 +1,25 @@
+<?php
+require("./mysql.inc.php");
+require("./foto-ch.inc.php");
+error_reporting(!(E_ALL));
+setlocale (LC_ALL, 'de_CH');
+$foto=strtolower($_REQUEST['s']);
+$l=strlen($foto);
+
+$sql="SELECT * FROM namen WHERE LOWER(nachname) LIKE '$foto%' ORDER BY nachname LIMIT 18";
+
+$result=mysql_query($sql);
+$c=0;
+//print_r($result);
+while(($c<12) && ($fetch=mysql_fetch_array($result))){
+//while($c<11){
+if (strtolower(substr($fetch['nachname'],0,$l))==$foto){
+	if ($fetch['zusatz']) $fetch['vorname'].=' '.$fetch['zusatz'];
+	$r=array($fetch['fotografen_id'],$fetch['nachname'],$fetch['vorname']);
+	echo ($r);
+	$c++;
+	}
+}
+jsonout($r);
+
+?>
