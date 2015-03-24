be97a07bdbda278475eb236fd855b70d8494f860
chrigu <chrigu@foto-ch.ch>
2015-03-24 10:24:42 +0000

    autologin from localStorage

--- fotofe/angular/app/app.module.js
+++ fotofe/angular/app/app.module.js
@@ -6,12 +6,28 @@ var app = angular.module('fotochWebApp', [
     'yaru22.md'
 ]);
 
-app.run(function($rootScope) {
+app.run(function($rootScope, $http) {
     $rootScope.user = '';
     $rootScope.userLevel = '';
     $rootScope.authToken = '';
     $rootScope.lang = 'de';
     $rootScope.ApiUrl = 'https://www2.foto-ch.ch/api';
+    var token=window.sessionStorage.authToken;
+    if (token!==undefined){
+		  $http.get($rootScope.ApiUrl+'/?a=user&b=info&token='+token).success (function(data){
+				var resp = data;
+				console.log(data);
+				if (data!==0){
+				$rootScope.user = data.user;
+				$rootScope.userLevel = parseInt(data.level);
+				$rootScope.authToken = token;
+				$http.defaults.headers.common['X-AuthToken']=token;
+				//$window.sessionStorage.authToken=undefined;
+				}
+
+		  });
+    }
+
 })
 
 app.service('fotochService', ['$http', function ($http) {
