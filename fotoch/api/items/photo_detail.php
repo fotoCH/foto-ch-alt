aab77601a96a0baeceb89ef9b0743bab59cc1e11
chrigu <chrigu@foto-ch.ch>
2015-06-23 15:10:36 +0000

    photo_details

---
+++ fotoch/api/items/photo_detail.php
@@ -0,0 +1,37 @@
+<?php
+
+$id=$_GET['id'];
+
+if (auth_level(USER_WORKER)){
+	$result=mysql_query("SELECT * FROM fotos WHERE id=$id");
+	$afields=array("id","typ","notiz");
+	
+} else {
+	$result=mysql_query("SELECT * FROM fotos WHERE id=$id");
+	$afields=array();
+}
+
+$select = 'f.id AS id, f.dc_created, f.zeitraum AS created, f.dc_title AS title, f.dc_description AS description, image_path, ';
+$select.= 'CONCAT(n.vorname, " ", n.nachname) AS name, ';
+$select.= 'i.name AS institution, ';
+$select.= 'b.name AS stock';
+
+$join .= "LEFT JOIN namen AS n ON f.dc_creator=n.fotografen_id ";
+$join .= "LEFT JOIN institution AS i ON f.edm_dataprovider=i.id ";
+$join .= "LEFT JOIN bestand AS b ON f.dcterms_ispart_of=b.id ";
+
+$query="SELECT DISTINCT $select FROM fotos AS f $join";
+
+	$query.=" WHERE f.id=$id";
+
+//echo $query;
+$result=mysql_query($query);
+$rowCount = mysql_num_rows($result);
+$out['result_count']= $rowCount;
+
+// do query
+$fetch = mysql_fetch_array ( $result );
+$out=$fetch;
+    
+jsonout($out);
+?>
