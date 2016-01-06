aae94fc6488e7e2ee6de92ba2197942d9d0e9505
chrigu <chrigu@foto-ch.ch>
2016-01-06 14:59:00 +0000

    api für fotos mit georef

--- fotoch/api/items/orte.php
+++ fotoch/api/items/orte.php
@@ -1,6 +1,9 @@
 <?php
-
-$query = "SELECT * FROM arbeitsorte WHERE lat<>0";
+if ($_GET ['photos']!="1")
+    $query = "SELECT *, '' as dc_right, '' as image_path FROM arbeitsorte WHERE lat<>0";
+else 
+    $query = "SELECT *, CONCAT(dcterms_spatial,': ', dc_title) as name, 'fotoquery' as swissname FROM fotos WHERE lat<>0";
+    
 
 $res=getfromselect($query);
 
