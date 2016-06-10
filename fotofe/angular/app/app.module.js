ab23823f6ae620a3b882126ba5de73813666b4d3
zivi <zivi@foto-ch.ch>
2016-06-10 06:46:19 +0000

    changing from localhost to www2.foto-ch.ch

--- fotofe/angular/app/app.module.js
+++ fotofe/angular/app/app.module.js
@@ -27,10 +27,10 @@ app.run(function($rootScope, $http, $location, $q, languages, $cacheFactory) {
     }
     
     // Development Server API URL
-    $rootScope.ApiUrl = 'http://localhost/fotoch/api';
+    //$rootScope.ApiUrl = 'http://localhost/fotoch/api';
 
     // Production Server API URL
-    //$rootScope.ApiUrl = 'https://www2.foto-ch.ch/api';
+    $rootScope.ApiUrl = 'https://www2.foto-ch.ch/api';
 
     
     var token=window.sessionStorage.authToken;
