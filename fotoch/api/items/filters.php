9c9a742add6e63987bc6ca7f452b57b2069cfaf8
zivi <zivi@foto-ch.ch>
2016-08-25 15:24:22 +0000

    added filter

--- fotoch/api/items/filters.php
+++ fotoch/api/items/filters.php
@@ -1,115 +1,156 @@
 <?php
 
 $filters = new Filters();
-if(array_key_exists('type', $_GET)) {
+if (array_key_exists('type', $_GET)) {
     $filters->output($_GET['type']);
 } else {
     $filters->output('No type set.');
 }
 
 
-class Filters {
+class Filters
+{
     private $filters = array();
     private $type = false;
 
-    private function unknown() {
-        $this->filters['type'] = 'Unknown Type: ' .$this->type;
+    private function unknown()
+    {
+        $this->filters['type'] = 'Unknown Type: ' . $this->type;
     }
 
-    private function fotografengattungen() {
+    private function fotografengattungen()
+    {
         $this->distinctFieldValues("fotografen.fotografengattungen_set", "fotografen");
     }
 
-    private function bildgattungen() {
+    private function bildgattungen()
+    {
         $this->distinctFieldValues("fotografen.bildgattungen_set", "fotografen");
     }
 
-    private function fotografenKanton() {
+    private function fotografenKanton()
+    {
         $this->distinctFieldValues("fotografen.kanton", "fotografen");
     }
 
-    private function institutionKanton() {
+    private function institutionKanton()
+    {
         $this->distinctFieldValues("institution.kanton", "institution", " WHERE institution.gesperrt = 0");
     }
 
-    private function institutionOrt() {
+    private function institution()
+    {
+        $this->distinctFieldValues("institution.name", "institution", " WHERE institution.gesperrt = 0");
+    }
+
+    private function institutionOrt()
+    {
         $this->distinctFieldValues("institution.ort", "institution", " WHERE institution.gesperrt = 0");
     }
 
-    private function ausstellungJahr() {
+    private function ausstellungJahr()
+    {
         $this->distinctFieldValues("ausstellung.jahr", "ausstellung", "", true);
     }
 
-    private function ausstellungTyp() {
+    private function ausstellungTyp()
+    {
         $this->distinctFieldValues("ausstellung.typ", "ausstellung");
     }
 
-    private function ausstellungOrt() {
+    private function ausstellungOrt()
+    {
         $this->distinctFieldValues("ausstellung.ort", "ausstellung");
     }
 
-    private function literaturJahr() {
+    private function literaturJahr()
+    {
         $this->distinctFieldValues("literatur.jahr", "literatur");
     }
 
-    private function literaturOrt() {
+    private function literaturOrt()
+    {
         $this->distinctFieldValues("literatur.ort", "literatur");
     }
 
-    private function photoStickworte() {
+    private function photoStickworte()
+    {
         $this->distinctFieldValues("dcterms_subject", "fotos");
     }
 
-    private function photoStocks() {
+    private function photoStocks()
+    {
         $this->filters['possible_values'] = array();
         $sql = "SELECT DISTINCT dcterms_ispart_of as 'value' FROM fotos";
         $set = mysql_query($sql);
-        while($row = mysql_fetch_assoc($set)) {
+        while ($row = mysql_fetch_assoc($set)) {
             $possible = $row['value'];
-            if(!array_key_exists($possible, $this->filters['possible_values'])) {
-                $stockQuery = "SELECT name FROM bestand WHERE id=".$possible;
+            if (!array_key_exists($possible, $this->filters['possible_values'])) {
+                $stockQuery = "SELECT name FROM bestand WHERE id=" . $possible;
                 $result = mysql_query($stockQuery);
-                while($stock = mysql_fetch_assoc($result)) {
+                while ($stock = mysql_fetch_assoc($result)) {
                     array_push($this->filters['possible_values'], array(
-                        "id" => $possible, 
+                        "id" => $possible,
                         "value" => $stock['name']
                     ));
                 }
             }
         }
-        if($desc) {
+        if ($desc) {
             rsort($this->filters['possible_values']);
         } else {
             sort($this->filters['possible_values']);
         }
     }
 
-    private function distinctFieldValues($field, $table, $where='', $desc = false) {
+    private function arbeitsorte()
+    {
+        $this->distinctFieldValues("arbeitsort", "arbeitsperioden");
+    }
+
+    private function verlag()
+    {
+        $this->distinctFieldValues("verlag", "literatur");
+    }
+
+    private function zeitraum()
+    {
+        $this->distinctFieldValues("zeitraum", "fotos");
+    }
+
+    private function spatial()
+    {
+        $this->distinctFieldValues("dcterms_spatial", "fotos");
+    }
+
+    private function distinctFieldValues($field, $table, $where = '', $desc = false)
+    {
         $this->filters['possible_values'] = array();
-        $sql = "SELECT DISTINCT ".$field." as 'value' FROM ".$table.$where;
+        $sql = "SELECT DISTINCT " . $field . " as 'value' FROM " . $table . $where;
         $set = mysql_query($sql);
-        while($row = mysql_fetch_assoc($set)) {
+        while ($row = mysql_fetch_assoc($set)) {
             $possibles = explode(",", $row['value']);
-            foreach($possibles as $poss) {
+            foreach ($possibles as $poss) {
                 $poss = trim($poss);
-                if($poss == '') {
+                if ($poss == '') {
                     continue;
                 }
-                if(!in_array($poss, $this->filters['possible_values'])) {
+                if (!in_array($poss, $this->filters['possible_values'])) {
                     array_push($this->filters['possible_values'], $poss);
                 }
             }
         }
-        if($desc) {
+        if ($desc) {
             rsort($this->filters['possible_values']);
         } else {
             sort($this->filters['possible_values']);
         }
     }
 
-    public function output($type) {
+    public function output($type)
+    {
         $this->type = $type;
-        switch($this->type) {
+        switch ($this->type) {
             case 'fotografengattungen':
                 $this->fotografengattungen();
                 break;
@@ -118,6 +159,9 @@ class Filters {
                 break;
             case 'fotografen_kanton':
                 $this->fotografenKanton();
+                break;
+            case 'institution':
+                $this->institution();
                 break;
             case 'institution_kanton':
                 $this->institutionKanton();
@@ -145,6 +189,18 @@ class Filters {
                 break;
             case 'photo_stocks':
                 $this->photoStocks();
+                break;
+            case 'arbeitsorte':
+                $this->arbeitsorte();
+                break;
+            case 'verlag':
+                $this->verlag();
+                break;
+            case 'zeitraum':
+                $this->zeitraum();
+                break;
+            case 'photo_ort':
+                $this->spatial();
                 break;
             default :
                 $this->unknown();
