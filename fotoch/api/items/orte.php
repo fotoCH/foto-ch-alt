a6b395351b5d5acdecd9943f5f6d22333dbb6201
zivi <zivi@foto-ch.ch>
2016-08-31 11:14:29 +0000

    completely new version (OO) of orte.php

--- fotoch/api/items/orte.php
+++ fotoch/api/items/orte.php
@@ -1,19 +1,68 @@
 <?php
-//if ($_GET ['photos']!="1")
-    $query = "SELECT *, '' as dc_right, '' as image_path FROM arbeitsorte WHERE lat<>0";
-//else 
-    $query2 = "SELECT *, CONCAT(dcterms_spatial,': ', dc_title) as name, 'fotoquery' as swissname FROM fotos WHERE lat<>0";
-    
-if ($_GET ['land'] || $_GET['kanton']){
-$query.=' AND kanton=\''.getClean('kanton').'\' AND land=\''.getClean('land').'\'';
-}
 
-$res=getfromselect($query);
-if ($_GET['photo']=='0'){
-    $res2=array();
-} else {
-    $res2=getfromselect($query2);
-}
 
-jsonout(array_merge($res,$res2));
+$venue = new Venue($_GET);
+
+$photographers = $venue->getPhotographers();
+$photos = $venue->getPhotos();
+
+jsonout(array_merge($photographers, $photos));
+
+class Venue{
+
+    private $term;
+    private $searchPhotographers;
+    private $searchPhotos;
+
+    function __construct($get_array)
+    {
+        $this->term = isset($get_array['query']) ? mysql_escape_string($get_array['query']) : false;
+        $this->searchPhotographers = isset($get_array['photographer']) ? filter_var($get_array['photographer'], FILTER_VALIDATE_BOOLEAN) : true;
+        $this->searchPhotos = isset($get_array['photos']) ? filter_var($get_array['photos'], FILTER_VALIDATE_BOOLEAN) : true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPhotographers(){
+        if($this->searchPhotographers){
+            $query = "SELECT *, '' as dc_right, '' as image_path FROM arbeitsorte WHERE lat<>0";
+
+            if($this->term){
+               $query .= " AND name LIKE '%" . $this->term . "%'";
+            }
+
+            return $this->getResultasArray($query);
+
+        }else{
+            return array();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getPhotos(){
+        if($this->searchPhotos){
+            $query = "SELECT *, CONCAT(dcterms_spatial,': ', dc_title) as name, 'fotoquery' as swissname FROM fotos WHERE lat<>0";
+
+            if($this->term){
+                $query .= " AND dcterms_spatial LIKE '%" . $this->term . "%'";
+            }
+            return $this->getResultasArray($query);
+        }else{
+            return array();
+        }
+    }
+
+    /**
+     * @param $query
+     * @return array
+     */
+    private function getResultasArray($query){
+        $result = getfromselect($query);
+        return $result ? $result : array();
+    }
+
+}
 ?>
