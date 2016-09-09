44b93515e898af3a6c2920739291bb5a05eb7f9f
zivi <zivi@foto-ch.ch>
2016-09-09 15:04:56 +0000

    change user stocks

--- fotoch/api/items/usermanagement.php
+++ fotoch/api/items/usermanagement.php
@@ -3,7 +3,7 @@
 $userManagement = new UserManagement();
 
 if ($userManagement->allowed()) {
-    if($userManagement->checkRequest()) {
+    if ($userManagement->checkRequest()) {
         $userManagement->request();
     } else {
         $userManagement->invalid();
@@ -19,25 +19,25 @@ class UserManagement
 
     private function getUsers()
     {
-         /*
-        SELECT u.username AS username, u.vorname AS vorname, u.nachname AS nachname, u.inst_comment AS inst_comment, u.level AS level, GROUP_CONCAT( bu.bestand_id
-        SEPARATOR  ',' ) AS stocks
-        FROM users u
-        JOIN bestand_users bu ON bu.user_id = u.id
-        ORDER BY nachname, vorname
-        */
+        /*
+       SELECT u.username AS username, u.vorname AS vorname, u.nachname AS nachname, u.inst_comment AS inst_comment, u.level AS level, GROUP_CONCAT( bu.bestand_id
+       SEPARATOR  ',' ) AS stocks
+       FROM users u
+       JOIN bestand_users bu ON bu.user_id = u.id
+       ORDER BY nachname, vorname
+       */
         $query_user = "SELECT * FROM users u ORDER BY nachname, vorname";
         $result_user = getfromselect($query_user);
 
         $query_stocks = "SELECT b.name as name, bu.user_id as user_id, b.id as id FROM bestand b JOIN bestand_users bu ON bu.bestand_id = b.id";
         $result_stocks = getfromselect($query_stocks);
 
-        foreach($result_user as $userKey => $user){
+        foreach ($result_user as $userKey => $user) {
             $result_user[$userKey]['stocks'] = array();
-            if(count($result_stocks) > 0){
+            if (count($result_stocks) > 0) {
                 // only iterate over stocks if left
-                foreach ($result_stocks as $key => $relatedStock){
-                    if($relatedStock['user_id'] == $user['id']){
+                foreach ($result_stocks as $key => $relatedStock) {
+                    if ($relatedStock['user_id'] == $user['id']) {
                         $result_user[$userKey]['stocks'][] = array('name' => $relatedStock['name'], 'id' => $relatedStock['id']);
                         // remove stock if realtion is set
                         unset($result_stocks[$key]);
@@ -48,8 +48,8 @@ class UserManagement
         jsonout($result_user);
     }
 
-    private function getStocks(){
-
+    private function getStocks()
+    {
         $stocks = array();
 
         // copied from filters->photoStocks (only stocks with photos)
@@ -71,34 +71,37 @@ class UserManagement
         jsonout($stocks);
     }
 
-    private function changeUserLevel(){
-        if(is_numeric($_GET['id']) && is_numeric($_GET['level'])) {
+    private function changeUserLevel()
+    {
+        if (is_numeric($_GET['id']) && is_numeric($_GET['level'])) {
             $update_query = "UPDATE users SET level=" . $_GET['level'] . " WHERE id=" . $_GET['id'];
 
-            mysql_query($update_query) ? jsonout(array('changeUserLevel' => 'success')) : jsonout(array('changeUserLevel' => 'database error (' . mysql_error() . ')')) ;
+            mysql_query($update_query) ? jsonout(array('changeUserLevel' => 'success')) : jsonout(array('changeUserLevel' => 'database error (' . mysql_error() . ')'));
             return;
-        }else{
-            jsonout(array('changeUserLevel' =>  'Paremeter are not numeric or not set.'));
+        } else {
+            jsonout(array('changeUserLevel' => 'Paremeter are not numeric or not set.'));
             return;
         }
     }
 
-    private function deleteUser(){
-        if(is_numeric($_GET['id'])) {
+    private function deleteUser()
+    {
+        if (is_numeric($_GET['id'])) {
             $delete_query = "DELETE FROM users WHERE id=" . $_GET['id'];
 
             $delete_relations_query = "DELETE FROM bestand_users WHERE user_id=" . $_GET['id'];
             mysql_query($delete_relations_query);
 
-            mysql_query($delete_query) ? jsonout(array('deleteUser' => 'success')) : jsonout(array('deleteUser' => 'database error (' . mysql_error() . ')')) ;
+            mysql_query($delete_query) ? jsonout(array('deleteUser' => 'success')) : jsonout(array('deleteUser' => 'database error (' . mysql_error() . ')'));
             return;
-        }else{
-            jsonout(array('deleteUser' =>  'Paremeter are not numeric or not set.'));
+        } else {
+            jsonout(array('deleteUser' => 'Paremeter are not numeric or not set.'));
             return;
         }
     }
 
-    private function addUser(){
+    private function addUser()
+    {
 
         // todo POST request!
         /*
@@ -120,24 +123,48 @@ class UserManagement
         $query .= implode(', ', array_keys($userData)) . ")";
         $query .= " VALUES('" . implode("', '", $userData) . "')";
 
-        if(count($stocks) > 0){
+        if (count($stocks) > 0) {
             $insert_result = mysql_query($query);
-            if($insert_result){
+            if ($insert_result) {
                 $user_id = mysql_fetch_assoc(mysql_query("SELECT id FROM users WHERE username='" . $userData['username'] . "' LIMIT 1"))['id'];
                 $stocks_query = "INSERT INTO bestand_users (bestand_id, user_id)";
                 $stocks_values = array();
-                foreach ($stocks as $stock){
+                foreach ($stocks as $stock) {
                     $stocks_values[] = "(" . $stock . "," . $user_id . ")";
                 }
                 $stocks_query .= " VALUES " . implode(',', $stocks_values) . ";";
                 return mysql_query($stocks_query) ? jsonout(array('addUser' => 'success')) : jsonout(array('addUser' => 'database error: ' . mysql_error()));
-            }else{
+            } else {
                 jsonout(array('addUser' => 'database error: ' . mysql_error()));
             }
-        }else{
+        } else {
             return mysql_query($query) ? jsonout(array('addUser' => 'success')) : jsonout(array('addUser' => 'database error: ' . mysql_error()));
         }
 
+    }
+
+
+    private function changeStocks()
+    {
+        $stocks = array_filter(explode(',', $_REQUEST['stocks']));
+        $userId = $_REQUEST['user'];
+
+        if (count($stocks) > 0 && is_numeric($userId)) {
+            // delete former relations
+            $delete_query = "DELETE FROM bestand_users WHERE user_id=" . $userId;
+            if (mysql_query($delete_query)) {
+                // insert new relations
+                $stocks_query = "INSERT INTO bestand_users (bestand_id, user_id)";
+                $stocks_values = array();
+                foreach ($stocks as $stock) {
+                    $stocks_values[] = "(" . $stock . "," . $userId . ")";
+                }
+                $stocks_query .= " VALUES " . implode(',', $stocks_values) . ";";
+                return mysql_query($stocks_query) ? jsonout(array('changeStocks' => 'success')) : jsonout(array('changeStocks' => 'database error: ' . mysql_error()));
+            } else {
+                return jsonout(array('changeStocks' => 'database error: ' . mysql_error()));
+            }
+        }
     }
 
     /**
@@ -151,6 +178,9 @@ class UserManagement
             switch ($this->action) {
                 case 'getUsers':
                     $this->getUsers();
+                    break;
+                case 'changeStocks':
+                    $this->changeStocks();
                     break;
                 case 'changeUserLevel':
                     $this->changeUserLevel();
@@ -173,8 +203,9 @@ class UserManagement
         }
     }
 
-    public function checkRequest() {
-        if( array_key_exists( 'action' , $_GET)) {
+    public function checkRequest()
+    {
+        if (array_key_exists('action', $_GET)) {
             $this->action = $_GET['action'];
             return true;
         }
@@ -199,7 +230,8 @@ class UserManagement
         ));
     }
 
-    public function invalid() {
+    public function invalid()
+    {
         jsonout(array(
             "request" => "invalid",
             "error" => $this->error
