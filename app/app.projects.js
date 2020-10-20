3466c8419e17cf2a03eb05298f006296c74efa59
raeffu <raeffu@raeffu.ch>
2020-10-20 11:46:12 +0200

    Show only text (from editor) in project view

--- app/app.projects.js
+++ app/app.projects.js
@@ -32,11 +32,6 @@ app.controller("ProjectDetailController", [
   "$timeout",
   "$stateParams",
   function($scope, $http, $rootScope, $window, $state, $timeout, $stateParams) {
-    $scope.detail = {
-      title: ""
-    };
-    $scope.imageRoot = $rootScope.imageRootUrl;
-
     $http
       .get(
         $rootScope.ApiUrl +
@@ -45,69 +40,6 @@ app.controller("ProjectDetailController", [
       )
       .success(function(data) {
         $scope.detail = data;
-
-        // images
-        $scope.detail.images = $scope.detail.images.split(",");
-        if ($scope.detail.images.length > 0 && $scope.detail.images[0] != "") {
-          angular.forEach($scope.detail.images, function(value, key) {
-            $http
-              .get($rootScope.ApiUrl + "/?a=photo&id=" + value)
-              .success(function(data) {
-                $scope.detail.images[key] = data;
-              });
-          });
-        }
-
-        // literature
-        $scope.detail.literature = $scope.detail.literature.split(",");
-        if (
-          $scope.detail.literature.length > 0 &&
-          $scope.detail.literature != ""
-        ) {
-          angular.forEach($scope.detail.literature, function(value, key) {
-            $http
-              .get($rootScope.ApiUrl + "/?a=literature&id=" + value)
-              .success(function(data) {
-                data.id = value;
-                $scope.detail.literature[key] = data;
-              });
-          });
-        }
-
-        // literature
-        $scope.detail.exhibitions = $scope.detail.exhibitions.split(",");
-        if (
-          $scope.detail.exhibitions.length > 0 &&
-          $scope.detail.exhibitions != ""
-        ) {
-          angular.forEach($scope.detail.exhibitions, function(value, key) {
-            $http
-              .get($rootScope.ApiUrl + "/?a=exhibition&id=" + value)
-              .success(function(data) {
-                data.id = value;
-                $scope.detail.exhibitions[key] = data;
-              });
-          });
-        }
-
-        // people
-        $scope.detail.people = $scope.detail.people.split(";");
-        if ($scope.detail.people.length > 0 && $scope.detail.people[0] != "") {
-          angular.forEach($scope.detail.people, function(value, key) {
-            var groups = $scope.detail.people[key].split(":");
-            $scope.detail.people[key] = {};
-            $scope.detail.people[key].title = groups[0];
-            $scope.detail.people[key].people = groups[1].split(",");
-            angular.forEach($scope.detail.people[key].people, function(v, k) {
-              $http
-                .get($rootScope.ApiUrl + "/?a=photographer&id=" + v)
-                .success(function(data) {
-                  data.id = v;
-                  $scope.detail.people[key].people[k] = data;
-                });
-            });
-          });
-        }
       });
   }
 ]);
